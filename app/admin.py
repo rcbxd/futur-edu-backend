b04c4213b3192f9cbe9fa140b3dfe003d6bbf6f8
rcbxd <rbcxdd@gmail.com>
2020-11-10 23:46:28 -0600

    added more fields to the models

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,6 @@
-from .models import Card, Topic
+from .models import Card, Topic, Category
 from django.contrib import admin
 
 admin.site.register(Card)
 admin.site.register(Topic)
+admin.site.register(Category)
--- app/models.py
+++ app/models.py
@@ -3,14 +3,23 @@ from django.contrib.auth.models import User
 import uuid
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, default="")
+    id_name = models.CharField(max_length=32, unique=True, default="")
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+
 class Card(models.Model):
     name = models.CharField(max_length=200)
     id_name = models.CharField(max_length=32, unique=True, default="")
-    description = models.CharField(default="", max_length=1000)
-    prerequisites = models.TextField(default="")
+    prerequisites = models.CharField(default="", max_length=1000)
     created = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey(
-        User, default=None, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        Category, default=None, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, default=None, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['created']
@@ -21,6 +30,8 @@ class Topic(models.Model):
     id_name = models.CharField(max_length=32, unique=True, default="")
     content = models.TextField(default="")
     created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(
+        Category, default=None, on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['created']
