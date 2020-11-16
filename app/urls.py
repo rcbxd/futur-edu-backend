02299ee8a4e9869698fa62b0789b73d02d8df02e
rcbxd <rbcxdd@gmail.com>
2020-11-15 22:15:56 -0600

    added category detail and list urls

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,11 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('cards/', views.card_list),
-    path('cards/<int:pk>', views.card_detail),
+    path('cards/<str:pk>', views.card_detail),
     path('users/', views.UserList.as_view()),
-    path('users/<int:pk>', views.UserDetail.as_view()),
+    path('users/<str:pk>', views.UserDetail.as_view()),
+    path('category/<str:pk>', views.category_detail),
+    path('categories/', views.CategoryList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
